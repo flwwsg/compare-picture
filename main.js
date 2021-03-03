6cc6ef4d577671b6c68cb922ddc9ae12dd27bd6a
flwwsg <flwwsg@gmail.com>
2021-03-03 15:29:23 +0000

    use img-ssim

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-const { ssim } = require('ssim.js');
-const loadImage = require('./loadImage');
+// const { ssim } = require('ssim.js');
+// const loadImage = require('./loadImage');
+const imgSSIM = require('img-ssim');
 
 // compare with ssim.js
 function compareWithSsim() {
-    const p1 = loadImage('./4ae7014e-b3ab-41fd-a416-abbe276f5b1a.png');
-    const p2 = loadImage('./4af88fe1-a2af-4e7a-85a5-ca94d9e12da8.png');
-    const { mssim, performance } = ssim(p1, p2);
-    console.log(`SSIM: ${mssim} (${performance}ms)`);
+    const p1 = './4ae7014e-b3ab-41fd-a416-abbe276f5b1a.png';
+    const p2 = './4af88fe1-a2af-4e7a-85a5-ca94d9e12da8.png';
+//    const { mssim, performance } = ssim(p1, p2);
+//    console.log(`SSIM: ${mssim} (${performance}ms)`);
+    imgSSIM(
+	    p1,
+	    p2,
+	    { enforceSameSize: false, resize: true },
+	    (err, s) => {
+		    console.log(err || s);
+	    }
+    );
 }
 
 compareWithSsim();
