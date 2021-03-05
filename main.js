fa824b3a3fbd199665c22fd3b1d48c14ab269fc8
flwwsg <flwwsg@qq.com>
2021-03-05 22:19:48 +0800

    wait

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const path = require('path');
 const fs = require("fs");
 const { fileExists, mkDir } = require('node-utils/files');
 
+let working = false;
 // compare with ssim.js
 function compareWithSsim(p1, p2, cb) {
+	working = true;
     imgSSIM(
 	    p1,
 	    p2,
@@ -25,15 +27,16 @@ function compareWithSsim(p1, p2, cb) {
 const imageDir = path.join(__dirname, 'images');
 const category = path.join(__dirname, 'category');
 mkDir(category);
-const allFiles = fs.readdirSync(imageDir).slice(0, 10);
+const allFiles = fs.readdirSync(imageDir);
 for (let i = 0; i < allFiles.length-1; i++) {
 	const srcFile = path.join(imageDir, allFiles[i]);
 	console.log('compare', srcFile);
-	allFiles.slice(i+1, allFiles.length).forEach(f => {
-		const targetFile = path.join(imageDir, f);
+	for (let j = i + 1; j < allFiles.length; j++) {
+		const targetFile = path.join(imageDir, allFiles[j]);
 		const cb = (srcFile, targetFile) => {
 			return (err, s) => {
 				if (err) {
+					working = false;
 					console.error(err);
 					return 0;
 				}
@@ -48,33 +51,12 @@ for (let i = 0; i < allFiles.length-1; i++) {
 						fs.copyFileSync(srcFile, path.join(categoryDir, path.basename(srcFile)));
 					}
 				}
+				working = false;
 				return s;
 			}
 		}
+		while (working) {
+		}
 		compareWithSsim(srcFile, targetFile, cb(srcFile, targetFile));
-	})
-	// for (let j = i + 1; j < allFiles.length; j++) {
-	// 	const targetFile = path.join(imageDir, allFiles[j]);
-	// 	const cb = (srcFile, targetFile) => {
-	// 		return (err, s) => {
-	// 			if (err) {
-	// 				console.error(err);
-	// 				return 0;
-	// 			}
-	// 			console.log('similar degree', s);
-	// 			if (s >= 0.5) {
-	// 				const baseName = path.basename(srcFile, '.png');
-	// 				const categoryDir = path.join(category, baseName);
-	// 				mkDir(categoryDir);
-	// 				// 目标文件复制到 category 目录中
-	// 				fs.copyFileSync(targetFile, path.join(categoryDir, path.basename(targetFile)));
-	// 				if (!fileExists(path.join(categoryDir, path.basename(srcFile)))) {
-	// 					fs.copyFileSync(srcFile, path.join(categoryDir, path.basename(srcFile)));
-	// 				}
-	// 			}
-	// 			return s;
-	// 		}
-	// 	}
-	// 	compareWithSsim(srcFile, targetFile, cb(srcFile, targetFile));
-	// }
+	}
 }
