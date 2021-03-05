bb661e6b0db7ccc3e76a2791d1c05385b95c2a3b
flwwsg <flwwsg@qq.com>
2021-03-05 22:37:53 +0800

    add await

--- main.js
+++ main.js
@@ -27,31 +27,39 @@ const imageDir = path.join(__dirname, 'images');
 const category = path.join(__dirname, 'category');
 mkDir(category);
 const allFiles = fs.readdirSync(imageDir);
-for (let i = 0; i < allFiles.length-1; i++) {
-	const srcFile = path.join(imageDir, allFiles[i]);
-	console.log('compare', srcFile);
-	for (let j = i + 1; j < allFiles.length; j++) {
-		const targetFile = path.join(imageDir, allFiles[j]);
-		const cb = (srcFile, targetFile) => {
-			return (err, s) => {
-				if (err) {
-					console.error(err);
-					return 0;
-				}
-				console.log('similar degree', s);
-				if (s >= 0.5) {
-					const baseName = path.basename(srcFile, '.png');
-					const categoryDir = path.join(category, baseName);
-					mkDir(categoryDir);
-					// 目标文件复制到 category 目录中
-					fs.copyFileSync(targetFile, path.join(categoryDir, path.basename(targetFile)));
-					if (!fileExists(path.join(categoryDir, path.basename(srcFile)))) {
-						fs.copyFileSync(srcFile, path.join(categoryDir, path.basename(srcFile)));
+async function main() {
+	for (let i = 0; i < allFiles.length-1; i++) {
+		const srcFile = path.join(imageDir, allFiles[i]);
+		console.log('compare', srcFile);
+		for (let j = i + 1; j < allFiles.length; j++) {
+			const targetFile = path.join(imageDir, allFiles[j]);
+			const cb = (srcFile, targetFile) => {
+				return (err, s) => {
+					if (err) {
+						console.error(err);
+						return 0;
 					}
+					console.log('similar degree', s);
+					if (s >= 0.5) {
+						const baseName = path.basename(srcFile, '.png');
+						const categoryDir = path.join(category, baseName);
+						mkDir(categoryDir);
+						// 目标文件复制到 category 目录中
+						fs.copyFileSync(targetFile, path.join(categoryDir, path.basename(targetFile)));
+						if (!fileExists(path.join(categoryDir, path.basename(srcFile)))) {
+							fs.copyFileSync(srcFile, path.join(categoryDir, path.basename(srcFile)));
+						}
+					}
+					return s;
 				}
-				return s;
 			}
+			await compareWithSsim(srcFile, targetFile, cb(srcFile, targetFile));
 		}
-		await compareWithSsim(srcFile, targetFile, cb(srcFile, targetFile));
 	}
 }
+
+main().then(res => {
+	console.log(res);
+}).catch(err => {
+	console.error(err);
+})
