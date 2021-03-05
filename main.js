61797e5f347c9ed61c76802770f7ee2f3648ba97
flwwsg <flwwsg@qq.com>
2021-03-05 21:56:55 +0800

    first compare

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const imgSSIM = require('img-ssim');
+const path = require('path');
+const fs = require("fs");
+const { fileExists, mkDir } = require('node-utils/files');
 
 // compare with ssim.js
 function compareWithSsim(p1, p2, cb) {
@@ -18,14 +21,35 @@ function compareWithSsim(p1, p2, cb) {
 	    }
     );
 }
-const p1 = './4ae7014e-b3ab-41fd-a416-abbe276f5b1a.png';
-const p2 = './4af88fe1-a2af-4e7a-85a5-ca94d9e12da8.png';
-const cb = (err, s) => {
-	if (err) {
-		console.error(err);
-		return 0;
+
+const imageDir = path.join(__dirname, 'images');
+const category = path.join(__dirname, 'category');
+mkDir(category);
+const allFiles = fs.readdirSync(imageDir);
+console.log(allFiles);
+for (let i = 0; i < allFiles.length-1; i++) {
+	const srcFile = path.join(imageDir, allFiles[i]);
+	for (let j = i + 1; j < allFiles.length; j++) {
+		const targetFile = path.join(imageDir, allFiles[j]);
+		const cb = (srcFile, targetFile) => {
+			return (err, s) => {
+				if (err) {
+					console.error(err);
+					return 0;
+				}
+				console.log('similar degree', s);
+				if (s >= 0.9) {
+					const baseName = path.basename(srcFile, '.png');
+					const categoryDir = path.join(category, baseName);
+					mkDir(categoryDir);
+					// 目标文件复制到 category 目录中
+					fs.copyFileSync(targetFile, path.join(categoryDir, path.basename(targetFile)));
+					if (!fileExists(path.join(categoryDir, path.basename(srcFile)))) {
+						fs.copyFileSync(srcFile, path.join(categoryDir, path.basename(srcFile)));
+					}
+				}
+			}
+		}
+		compareWithSsim(srcFile, targetFile, cb(srcFile, targetFile));
 	}
-	console.log(s);
-	return s;
 }
-compareWithSsim(p1, p2, cb);
