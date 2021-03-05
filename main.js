ae085a5ce4eaaea952de3a9cc914fb6c2cd6b80a
flwwsg <flwwsg@qq.com>
2021-03-05 23:07:28 +0800

    publish

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function compareWithSsim(p1, p2, cb) {
 const imageDir = path.join(__dirname, 'images');
 const category = path.join(__dirname, 'category');
 mkDir(category);
-const allFiles = fs.readdirSync(imageDir).slice(0, 10);
+const allFiles = fs.readdirSync(imageDir);
 function main() {
 	const go = function (i) {
 		if (i < allFiles.length -1) {
@@ -67,34 +67,6 @@ function main() {
 		}
 	}
 	go(0);
-	// for (let i = 0; i < allFiles.length-1; i++) {
-	// 	const srcFile = path.join(imageDir, allFiles[i]);
-	// 	console.log('compare', srcFile);
-	// 	for (let j = i + 1; j < allFiles.length; j++) {
-	// 		const targetFile = path.join(imageDir, allFiles[j]);
-	// 		const cb = (srcFile, targetFile) => {
-	// 			return (err, s) => {
-	// 				if (err) {
-	// 					console.error(err);
-	// 					return 0;
-	// 				}
-	// 				console.log('similar degree', s);
-	// 				if (s >= 0.9) {
-	// 					const baseName = path.basename(srcFile, '.png');
-	// 					const categoryDir = path.join(category, baseName);
-	// 					mkDir(categoryDir);
-	// 					// 目标文件复制到 category 目录中
-	// 					fs.copyFileSync(targetFile, path.join(categoryDir, path.basename(targetFile)));
-	// 					if (!fileExists(path.join(categoryDir, path.basename(srcFile)))) {
-	// 						fs.copyFileSync(srcFile, path.join(categoryDir, path.basename(srcFile)));
-	// 					}
-	// 				}
-	// 				return s;
-	// 			}
-	// 		}
-	// 		compareWithSsim(srcFile, targetFile, cb(srcFile, targetFile));
-	// 	}
-	// }
 }
 
 main();
