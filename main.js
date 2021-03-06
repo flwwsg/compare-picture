26ff745190075a7f0a22c5f7c6320df4bf145831
flwwsg <flwwsg@qq.com>
2021-03-06 12:53:15 +0800

    test concurrent process

--- main.js
+++ main.js
@@ -26,44 +26,55 @@ function compareWithSsim(p1, p2, cb) {
 const imageDir = path.join(__dirname, 'images');
 const category = path.join(__dirname, 'category');
 mkDir(category);
-const allFiles = fs.readdirSync(imageDir);
+const allFiles = fs.readdirSync(imageDir).slice(0, 20);
+let nextCount = 0;
+// 并发数
+const concurrent = 10;
 function main() {
 	const go = function (i) {
-		if (i < allFiles.length -1) {
-			// go on
-			const srcFile = path.join(imageDir, allFiles[i]);
-			const cmp = function (start, j) {
-				if (j >= allFiles.length) {
-					// 下一循环
-					return go(start+1);
-				}
-				console.log('compare', start, j);
-				const targetFile = path.join(imageDir, allFiles[j]);
-				const cb = (srcFile, targetFile) => {
-					return (err, s) => {
-						if (err) {
-							console.error(err);
+		nextCount ++;
+		if (nextCount % concurrent !== 0) {
+			// 不是最后一个完成的
+			return;
+		}
+		for (let next = i; next < concurrent + i; next++) {
+			if (next < allFiles.length -1) {
+				// go on
+				const srcFile = path.join(imageDir, allFiles[next]);
+				const cmp = function (start, j) {
+					if (j >= allFiles.length) {
+						// 下一循环
+						return go(start+1);
+					}
+					console.log('compare', start, j);
+					const targetFile = path.join(imageDir, allFiles[j]);
+					const cb = (srcFile, targetFile) => {
+						return (err, s) => {
+							if (err) {
+								console.error(err);
+								return cmp(start, j+1);
+							}
+							console.log('similar degree', s);
+							if (s >= 0.7) {
+								const baseName = path.basename(srcFile, '.png');
+								const categoryDir = path.join(category, baseName);
+								mkDir(categoryDir);
+								// 目标文件复制到 category 目录中
+								fs.copyFileSync(targetFile, path.join(categoryDir, path.basename(targetFile)));
+								if (!fileExists(path.join(categoryDir, path.basename(srcFile)))) {
+									fs.copyFileSync(srcFile, path.join(categoryDir, path.basename(srcFile)));
+								}
+							}
 							return cmp(start, j+1);
 						}
-						console.log('similar degree', s);
-						if (s >= 0.7) {
-							const baseName = path.basename(srcFile, '.png');
-							const categoryDir = path.join(category, baseName);
-							mkDir(categoryDir);
-							// 目标文件复制到 category 目录中
-							fs.copyFileSync(targetFile, path.join(categoryDir, path.basename(targetFile)));
-							if (!fileExists(path.join(categoryDir, path.basename(srcFile)))) {
-								fs.copyFileSync(srcFile, path.join(categoryDir, path.basename(srcFile)));
-							}
-						}
-						return cmp(start, j+1);
 					}
+					return compareWithSsim(srcFile, targetFile, cb(srcFile, targetFile));
 				}
-				return compareWithSsim(srcFile, targetFile, cb(srcFile, targetFile));
+				cmp(next, next+1);
+			} else {
+				console.log('finish', next);
+				break;
 			}
-			cmp(i, i+1);
-		} else {
-			console.log('finish', i);
 		}
 	}
 	go(0);
